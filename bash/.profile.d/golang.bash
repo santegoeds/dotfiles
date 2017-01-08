f0af3cbdde6bae76971982c89289b2a50a625260
Tjerk Santegoeds <tsan@tdias.com>
2017-01-08 12:04:48 +0100

    better bash env for golang.

--- bash/.profile.d/golang.bash
+++ bash/.profile.d/golang.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Ubuntu make installation of Go Lang
-GOROOT=/home/vagrant/.local/share/umake/go/go-lang
+GOROOT=$HOME/.local/share/umake/go/go-lang
 if [[ -d $GOROOT ]]; then
     export GOROOT
     export PATH=/home/vagrant/.local/share/umake/go/go-lang/bin:$PATH
 fi
+
+export GOVERSION=$(go version |awk '{ print $3; }')
+export GOPATH=$HOME/.local/share/gopath/$GOVERSION
+
+mkdir -p $GOPATH
+

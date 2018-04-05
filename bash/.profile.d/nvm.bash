834bd232f875fbe299039918700bdf535c1dea48
Tjerk Santegoeds <tsan@tdias.com>
2018-04-05 10:53:50 +0200

    nvm: add bash completion.

--- bash/.profile.d/nvm.bash
+++ bash/.profile.d/nvm.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 if [[ -d "$HOME/.nvm" ]]; then
     export NVM_DIR="$HOME/.nvm"
-    [[ -r "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh" # This loads nvm
+    # This loads nvm
+    [[ -r "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+    [[ -r "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
 fi
